5748a2346ffcd540c047705a6b1b5a3101ee7ea2
misogohan <soypasterice@gmail.com>
2022-07-12 15:36:57 +0900

    refactored some

--- avl_tree.h
+++ avl_tree.h
@@ -1,162 +1,210 @@
 #ifndef MISOLIB_AVL_TREE_H
 #define MISOLIB_AVL_TREE_H
 
-#include "node.h"
+#include <functional>
 
-namespace AVL {
-  template<typename T>
-  void left_rotate(Node<T> **target) {
-    Node<T> *left = *target;
-    Node<T> *right = left->right;
+template <typename T>
+inline static T max(const T &a, const T &b) { return a > b ? a : b; }
+template <typename T>
+static void chmax(T &a, const T &b) { if (b > a) a = b; }
 
-    *target = right;
-    left->right = right->left;
-    right->left = left;
+namespace misolib {
+  namespace AVL {
+    template<typename T>
+    struct Node {
+      Node<T> *left, *right;
+      T value;
+      int height;
 
-    left->calc_height();
-    right->calc_height();
-  }
+      Node(const T &val) : left(nullptr), right(nullptr), value(val), height(1) {}
 
-  template<typename T>
-  void right_rotate(Node<T> **target) {
-    Node<T> *right = *target;
-    Node<T> *left = right->left;
-
-    *target = left;
-    right->left = left->right;
-    left->right = right;
-
-    right->calc_height();
-    left->calc_height();
-  }
-
-  template<typename T>
-  void rotate(Node<T> **target) {
-    Node<T> *p = *target;
-    int bf = p->balance_factor();
-
-    if (bf < -1) {
-      if (p->right->balance_factor() > 0) right_rotate(&p->right);
-      left_rotate(target);
-      rotate(&(*target)->left);
-    } else if (bf > 1) {
-      if (p->left->balance_factor() < 0) left_rotate(&p->left);
-      right_rotate(target);
-      rotate(&(*target)->right);
-    }
-  }
-
-  template<typename T>
-  Node<T> *find(Node<T> **from, const T &x) {
-    Node<T> *p = *from;
-    if (p == nullptr) return nullptr;
-
-    if (p->value == x) return p;
-
-    Node<T> **next_target = &(Node<T>::less(p->value, x) ? p->left : p->right);
-    return find(next_target, x);
-  }
-
-  template<typename T>
-  void insert(Node<T> **target, const T &value) {
-    Node<T> *p = *target;
-
-    if (p == nullptr) {
-      *target = new Node<T>(value);
-      return;
-    }
-
-    Node<T> **next_target = &(value < p->value ? p->left : p->right);
-    insert(next_target, value);
-    (*target)->calc_height();
-    rotate(target);
-  }
-
-  template<typename T>
-  Node<T> *remove_first(Node<T> **target) {
-    Node<T> *p = *target;
-    // if (p == nullptr) return nullptr;
-    if (p->left == nullptr) {
-      *target = p->right;
-      return p;
-    } else {
-      Node<T> *res = remove_first(&p->left);
-      (*target)->calc_height();
-      rotate(target);
-      return res;
-    }
-  }
-
-  template<typename T>
-  void remove(Node<T> **target, const T &value) {
-    Node<T> *p = *target;
-    if (p == nullptr) return;
-
-    if (value == p->value) {
-      remove(&p->left, value);
-      remove(&p->right, value);
-
-      if (p->left == nullptr && p->right == nullptr) {
-        *target = nullptr;
-        delete p;
-      } else if (p->left != nullptr && p->right != nullptr) {
-        Node<T> *sub = remove_first(&p->right);
-        p->value = sub->value;
-        delete sub;
-        rotate(target);
-      } else {
-        Node<T> *next = p->left != nullptr ? p->left : p->right;
-        *target = next;
-        delete p;
+      int left_height() const {
+        if (left == nullptr) return 0;
+        return left->height;
       }
-      return;
-    } else {
+
+      int right_height() const {
+        if (right == nullptr) return 0;
+        return right->height;
+      }
+
+      int calc_height() {
+        height = max(left_height(), right_height()) + 1;
+        return height;
+      }
+
+      int deep_calc_height() {
+        height = 0;
+        if (left != nullptr) chmax(height, left->deep_calc_height());
+        if (right != nullptr) chmax(height, right->deep_calc_height());
+        return ++height;
+      }
+
+      int balance_factor() const {
+        return left_height() - right_height();
+      }
+
+      bool walk_forward(std::function<bool(const Node<T>&)> f) const {
+        return (
+             (this->left == nullptr || this->left->walk_forward(f))
+          && f(*this)
+          && (this->right == nullptr || this->right->walk_forward(f))
+        );
+      }
+      bool scan_forward(std::function<bool(const T&)> f) const {
+        return (
+             (this->left == nullptr || this->left->scan_forward(f))
+          && f(this->value)
+          && (this->right == nullptr || this->right->scan_forward(f))
+        );
+      }
+      bool walk_backward(std::function<bool(const Node<T>&)> f) const {
+        return (
+             (this->right == nullptr || this->right->walk_backward(f))
+          && f(*this)
+          && (this->left == nullptr || this->left->walk_backward(f))
+        );
+      }
+      bool scan_backward(std::function<bool(const T&)> f) const {
+        return (
+             (this->right == nullptr || this->right->scan_backward(f))
+          && f(this->value)
+          && (this->left == nullptr || this->left->scan_backward(f))
+        );
+      }
+    };
+
+
+
+    template<typename T>
+    void left_rotate(Node<T> **target) {
+      Node<T> *left = *target;
+      Node<T> *right = left->right;
+
+      *target = right;
+      left->right = right->left;
+      right->left = left;
+
+      left->calc_height();
+      right->calc_height();
+    }
+
+    template<typename T>
+    void right_rotate(Node<T> **target) {
+      Node<T> *right = *target;
+      Node<T> *left = right->left;
+
+      *target = left;
+      right->left = left->right;
+      left->right = right;
+
+      right->calc_height();
+      left->calc_height();
+    }
+
+    template<typename T>
+    void rotate(Node<T> **target) {
+      Node<T> *p = *target;
+      int bf = p->balance_factor();
+
+      if (bf < -1) {
+        if (p->right->balance_factor() > 0) right_rotate(&p->right);
+        left_rotate(target);
+        rotate(&(*target)->left);
+      } else if (bf > 1) {
+        if (p->left->balance_factor() < 0) left_rotate(&p->left);
+        right_rotate(target);
+        rotate(&(*target)->right);
+      }
+    }
+
+    template<typename T>
+    bool find(Node<T> **from, const T &x) {
+      Node<T> *p = *from;
+      if (p == nullptr) return false;
+
+      if (p->value == x) return true;
+
+      Node<T> **next_target = &(p->value < x ? p->left : p->right);
+      return find(next_target, x);
+    }
+
+    template<typename T>
+    void insert(Node<T> **target, const T &value) {
+      Node<T> *p = *target;
+
+      if (p == nullptr) {
+        *target = new Node<T>(value);
+        return;
+      }
+
       Node<T> **next_target = &(value < p->value ? p->left : p->right);
-      remove(next_target, value);
+      insert(next_target, value);
       (*target)->calc_height();
       rotate(target);
     }
-  }
-  template<typename T>
-  void remove(Node<T> **target) {
-    Node<T> *p = *target;
-    if (p == nullptr) return;
-    remove(&p->left);
-    remove(&p->right);
-    *target = nullptr;
-    delete p;
-  }
 
-  template <typename T>
-  void consume_lt(Node<T> **target, const T &base, void (*consumer)(const T &)) {
-    Node<T> *p = *target;
-    if (p == nullptr) return;
+    template<typename T>
+    Node<T> *pop_front(Node<T> **target) {
+      Node<T> *p = *target;
+      if (p == nullptr) return nullptr;
 
-    if (p->value < base) {
-      consume_lt(&p->left, base, consumer);
-      consumer(p->value);
-      consume_lt(&p->right, base, consumer);
-      *target = p->right;
-      delete p;
-    } else {
-      consume_lt(&p->left, base, consumer);
-      rotate(target);
+      if (p->left == nullptr) {
+        *target = p->right;
+        p->right = nullptr;
+        
+        return p;
+      } else {
+        Node<T> *res = pop_front(&p->left);
+
+        p->calc_height();
+        rotate(target);
+
+        return res;
+      }
     }
-  }
-  template <typename T>
-  void consume_gt(Node<T> **target, const T &base, void (*consumer)(const T &)) {
-    Node<T> *p = *target;
-    if (p == nullptr) return;
 
-    if (p->value > base) {
-      consume_gt(&p->right, base, consumer);
-      consumer(p->value);
-      consume_gt(&p->left, base, consumer);
-      *target = p->left;
+    template<typename T>
+    void remove(Node<T> **target) {
+      Node<T> *p = *target;
+      if (p == nullptr) return;
+      remove(&p->left);
+      remove(&p->right);
+      *target = nullptr;
       delete p;
-    } else {
-      consume_gt(&p->right, base, consumer);
-      rotate(target);
+    }
+
+    template <typename T>
+    void consume_lt(Node<T> **target, const T &base, std::function<void(const T &)> consumer) {
+      Node<T> *p = *target;
+      if (p == nullptr) return;
+
+      if (p->value < base) {
+        consume_lt(&p->left, base, consumer);
+        consumer(p->value);
+        consume_lt(&p->right, base, consumer);
+        *target = p->right;
+        delete p;
+      } else {
+        consume_lt(&p->left, base, consumer);
+        rotate(target);
+      }
+    }
+    template <typename T>
+    void consume_gt(Node<T> **target, const T &base, std::function<void(const T &)> consumer) {
+      Node<T> *p = *target;
+      if (p == nullptr) return;
+
+      if (base < p->value) {
+        consume_gt(&p->right, base, consumer);
+        consumer(p->value);
+        consume_gt(&p->left, base, consumer);
+        *target = p->left;
+        delete p;
+      } else {
+        consume_gt(&p->right, base, consumer);
+        rotate(target);
+      }
     }
   }
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,12 @@
+#include <cstdio>
 #include <iostream>
 #include "avl_tree.h"
 
+using namespace misolib;
+using namespace std;
+
 bool print(const int &v) {
-  std::printf(" %d", v);
+  printf(" %d", v);
   return true;
 }
 
@@ -10,7 +14,22 @@ int main() {
   AVL::Node<int> *tree = nullptr;
 
   for (int i = 1; i <= 15; ++i) AVL::insert(&tree, i);
-  tree->walk_forward(print);
+  tree->scan_forward(print);
+  puts("");
+  // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+
+  AVL::insert(&tree, 8);
+  tree->scan_forward(print);
+  puts("");
+  // 1 2 3 4 5 6 7 8 8 9 10 11 12 13 14 15
+
+  AVL::consume_lt<int>(&tree, 9, [](const int &v) { printf(" %d", v); });
+  puts("");
+  // 1 2 3 4 5 6 7 8 8
+
+  tree->scan_forward(print);
+  puts("");
+  // 9 10 11 12 13 14 15 
 
   AVL::remove(&tree);
 }
